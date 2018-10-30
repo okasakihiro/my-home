d48a60738c6a87a023a2861bfea13724cc6f6c0e
okasakihiro <okasakihiro@gmail.com>
2018-10-30 17:12:35 +0800

    新增了临时施工内容

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,34 +62,27 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+            .footer {
+                position: relative;
+                margin-bottom: 30px;
+            }
         </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
+                <div>
+                    <img src="images/global/under_construction.png" alt="施工中....">
+                </div>
                 <div class="title m-b-md">
-                    Laravel
+                    施工中....
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+            </div>
+            <div class="footer">
+                <span>
+                    Power By 水原西守歌
+                </span>
             </div>
         </div>
     </body>
