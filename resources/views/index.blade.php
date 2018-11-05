eeaddd3a815b4849200384144ecd9e57bbd3287a
okasakihiro <okasakihiro@gmail.com>
2018-11-05 22:05:29 +0800

    '新增了攒井进度页面'

---
+++ resources/views/index.blade.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ config('app.name') }}</title>
+</head>
+<body>
+    <div>
+        <h3>GBF井井计算器</h3>
+        <table>
+            <tr>
+                <td>宝晶石</td>
+                <td>
+                    <input id="store" type="text">
+                </td>
+            </tr>
+            <tr>
+                <td>单抽券</td>
+                <td>
+                    <input id="ticket" type="text">
+                </td>
+            </tr>
+            <tr>
+                <td>十连券</td>
+                <td>
+                    <input id="ten-ticket" type="text">
+                </td>
+
+            </tr>
+            <tr>
+                <td>宝晶石换算</td>
+                <td>
+                    <span></span>
+                    <span></span>
+                </td>
+            </tr>
+            <tr>
+                <td>白嫖券换算</td>
+                <td>
+                    <span></span>
+                    <span></span>
+                </td>
+            </tr>
+            <tr>
+                <td>可井百分比</td>
+                <td>
+                    <span></span>
+                </td>
+            </tr>
+            <tr>
+                <td>母猪石数量</td>
+                <td>
+                    <span></span>
+                </td>
+            </tr>
+            <tr>
+                <td>你还需要课</td>
+                <td>
+                    <span></span>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+</body>
+</html>
